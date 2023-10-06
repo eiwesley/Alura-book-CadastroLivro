f5c0686810aa2eecb1253257d479e985619f51ca
Wesley Medeiros <90868789+eiwesley@users.noreply.github.com>
2023-10-06 14:45:22 -0300

    :sparkles: feat: 3 sort de livros

--- app/metodoForEach.js
+++ app/metodoForEach.js
@@ -8,7 +8,7 @@ function exibirOsLivrosNaTela(listaDeLivros){
         <img class="livro__imagens" src="${[livro.imagem]}" alt="${livro.alt}" />
         <h2 class="livro__titulo">${livro.titulo}</h2>
         <p class="livro__descricao">${livro.autor}</p>
-        <p class="livro__preco" id="preco">${livro.preco.toFixed(2)}</p>
+        <p class="livro__preco" id="preco">R$ ${livro.preco.toFixed(2)}</p>
         <div class="tags">
           <span class="tag">${livro.categoria}</span>
         </div>
