a825d8ebb8484d4fbb08cf0d1dea4ecd5c8b97ca
Wesley Medeiros <90868789+eiwesley@users.noreply.github.com>
2023-10-06 15:42:10 -0300

    :sparkles: feat: 2 Manipulando HTML com JS

--- app/metodoFilter.js
+++ app/metodoFilter.js
@@ -1,10 +1,29 @@
 const botoes = document.querySelectorAll('.btn');
 botoes.forEach(btn => btn.addEventListener('click', filtrarLivros))
 
-function filtrarLivros(){
+function filtrarLivros() {
     const elementoBtn = document.getElementById(this.id);
-    const categoria = elementoBtn.value
-    let livrosFiltrados = livros.filter(livro => livro.categoria == categoria);
-    exibirOsLivrosNaTela(livrosFiltrados)
+    const categoria = elementoBtn.value;
+    let livrosFiltrados = categoria == 'disponivel' ? filtrarPorDisponibilidade() : filtrarPorCategodia(categoria);
+    exibirOsLivrosNaTela(livrosFiltrados);
+
+    if (categoria == 'disponivel') {
+        exibirValorTotalDosLivrosDisponiveisNaTela();
+    }
 }
 
+function filtrarPorCategodia(categoria) {
+    return livros.filter(livro => livro.categoria == categoria);
+}
+
+function filtrarPorDisponibilidade() {
+    return livros.filter(livro => livro.quantidade > 0);
+}
+
+function exibirValorTotalDosLivrosDisponiveisNaTela() {
+    elementoComValorTotalDeLivrosDisponiveis.innerHTML = `
+    <div class="livros__disponiveis">
+    <p>Todos os livros disponíveis por R$ <span id="valor">299,00</span></p>
+    </div>
+    `
+}
